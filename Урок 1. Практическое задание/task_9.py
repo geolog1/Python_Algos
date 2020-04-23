8fb0ec74d0877c034d9991118d170d02ab6571fb
geolog1 <ps.tolstov@gmail.com>
2020-04-23 15:41:17 +0300

    Не успел сделать №4 и №7 задания :( К сожалению, почти все задания Гуглил, т.к. не было основ Питона.

--- Урок 1. Практическое задание/task_9.py
+++ Урок 1. Практическое задание/task_9.py
@@ -4,3 +4,17 @@
 
 Подсказка: можно добавить проверку, что введены равные числа
 """
+
+
+
+print('Введите три числа: ')
+a = int(input())
+b = int(input())
+c = int(input())
+
+if b < a < c or c < a < b:
+    print('Среднее:', a)
+elif a < b < c or c < b < a:
+    print('Среднее:', b)
+else:
+    print('Среднее:', c)
