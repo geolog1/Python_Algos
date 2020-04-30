465499d8f9114acb3f510510312da69151bd058c
geolog1 <ps.tolstov@gmail.com>
2020-04-30 12:47:47 +0300

    рекурсия_№9

--- Урок 2. Практическое задание/task_9/task_9_2.py
+++ Урок 2. Практическое задание/task_9/task_9_2.py
@@ -10,3 +10,28 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+
+def recur(quantity, steps, highest_numb, max_sum):
+    # Рекурсия
+    summ = 0
+    try:
+        numb = input("Введите число: ")
+        for i in numb:
+            summ += int(i)
+        if summ > max_sum:
+            max_sum = summ
+            highest_numb = numb
+        steps += 1
+        if steps == quantity:
+            return f"Наибольшее число по сумме цифр: {highest_numb}, сумма его цифр {max_sum}"
+        else:
+            return recur(quantity, steps, highest_numb, max_sum)
+    except ValueError:
+        print("Вы ввели строку. Введите число!")
+
+STEPS = 0
+HIGHEST_NUMB = 0
+MAX_SUM = 0
+
+try:
+    QUANT = int(input("Введите количество чисел:"))
