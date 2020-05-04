6c1680c098e2404bee3540b55b9f5e5dc8bfee44
geolog1 <ps.tolstov@gmail.com>
2020-05-04 17:01:59 +0300

    task_7

--- Урок 3.Практическое задание/task_7.py
+++ Урок 3.Практическое задание/task_7.py
@@ -7,3 +7,24 @@
 Наименьший элемент: -86, встречается в этом массиве 1 раз
 Второй наименьший элемент: -73
 """
+
+
+from random import randint
+
+def func(lst_base):
+    print(f"Исходный массив: {lst_base}")
+
+    min_1 = min(lst_base)
+    count = 0
+    for el in lst_base:
+        if el == min_1:
+            lst_base.remove(el)
+            count += 1
+    print(f"Наименьший элемент: {min_1}, встречается в массиве {count} раз")
+
+    if count == 1:
+        min_2 = min(lst_base)
+        print(f'Второй наименьший элемент: {min_2}')
+
+LST_BASE = [randint(-100, 100) for i in range(40)]
+func(LST_BASE)
