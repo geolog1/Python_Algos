b2fe3c9e2726f1f3d09514e0e038966f0ee0482d
geolog1 <ps.tolstov@gmail.com>
2020-05-04 17:01:42 +0300

    task_6

--- Урок 3.Практическое задание/task_6.py
+++ Урок 3.Практическое задание/task_6.py
@@ -13,3 +13,38 @@
 Массив: [88, 58, 50, 77, 49, 6, 42, 67, 14, 79]
 Сумма элементов между минимальным (6)  и максимальным (88) элементами: 234
 """
+
+from random import randint
+
+def func(lst):
+    print(f"Массив: {lst}")
+
+    min_index = 0
+    max_index = 0
+    step = 1
+    common_sum = 0
+
+    for i in lst:
+        if lst[min_index] > i:
+            min_index = lst.index(i)
+        elif lst[max_index] < i:
+            max_index = lst.index(i)
+
+    if max_index - min_index < 0:
+         step = -1
+
+    for i in lst[min_index + step:max_index:step]:
+         common_sum += i
+
+    print(
+         f"Сумма элементов между минимальным ({lst[min_index]})"
+         f" и макимальным ({lst[max_index]}) элементами равна: {common_sum}"
+    )
+
+
+try:
+    NUM = int(input('Введите количество элементов в массиве: '))
+    LST = [randint(1, 100) for x in range(NUM)]
+    func(LST)
+except ValueError:
+    print("Вы ввели не число, а строку. Ошибка")
