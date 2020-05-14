7366ec1ae805eb6da18a4f7609cd3056d7e6c5b2
geolog1 <ps.tolstov@gmail.com>
2020-05-15 00:45:46 +0300

    Task_1

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,39 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+import timeit
+import random
+
+def bubble_sort(orig_list):
+    swap = True
+    while swap:
+        swap = False
+        for i in range(len(orig_list)-1):
+            if orig_list[i] < orig_list[i+1]:
+                orig_list[i], orig_list[i+1] = orig_list[i+1], orig_list[i]
+                swap = True
+
+    return orig_list
+
+
+orig_list = [random.randint(-1000, 1000) for _ in range(2000)]
+print("Исходный массив (рандом): ", orig_list)
+print("Отсортированный массив (рандом):", bubble_sort(orig_list))
+
+print(timeit.timeit("bubble_sort(orig_list)",
+    setup="from __main__ import bubble_sort, orig_list", number=1000))
+
+
+orig_list = [i for i in range(-1000, 1000)]
+orig_list.reverse()
+print("Исходный массив (изначально сортирован): ", orig_list)
+print("Отсортированный массив (изначально сортирован):", bubble_sort(orig_list))
+
+print(timeit.timeit("bubble_sort(orig_list)",
+    setup="from __main__ import bubble_sort, orig_list", number=1000))
+
+"""
+Не до конца уверен, что алгоритм верный,
+так как разных объемах данных иногда для отсортированного списка время получается больше.
+
+"""
